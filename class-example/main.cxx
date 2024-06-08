d9316237671056eaddd044bf4841768eb2d93398
Tommaso <55707296+tompedro@users.noreply.github.com>
2024-06-08 17:59:42 +0200

    Delete class-example/main.cxx

--- class-example/main.cxx
+++
@@ -1,7 +0,0 @@
-#include "Complesso.h"
-#include <iostream>
-int main() {
-	std::cout << "Hello" << std::endl;
-	Complesso b(7,2); // equivale a: Complesso b = Complesso::Complesso(7, 2);
-	return 0;
-}
